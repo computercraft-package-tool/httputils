41c8f49a632b3c526d0859f7751fb7e6a0f05769
PentagonLP <85071935+PentagonLP@users.noreply.github.com>
2025-05-30 17:07:18 +0200

    New include method, update dependencies, bump version
    Remove unneeded ProperPrint dependency, set FileUtils dependency to new version

--- httputils.lua
+++ httputils.lua
@@ -1,16 +1,22 @@
 --[[ 
 	HttpUtils Library for ComputerCraft
 	Authors: PentagonLP, SkyTheCodeMaster
-	Version: 1.0
+	Version: 1.1
 ]]
 
-os.loadAPI("/lib/fileutils")
+--[[ Include FileUtils as dependency
+]]
+local fileutils = dofile("lib/fileutils.lua")
+
+--[[ HttpUtils library module
+]]
+local httputils = {}
 
 --[[ Gets result of HTTP URL
 	@param String url: The desired URL
 	@return Table|boolean result|error: The result of the request; If the URL is not reachable, an error is printed in the terminal and boolean false is returned
---]]
-function gethttpresult(url)
+]]
+function httputils.gethttpresult(url)
 	if not http.checkURL(url) then
 		print("ERROR: Url '" .. url .. "' is blocked in config. Unable to fetch data.")
 		return false
@@ -26,9 +32,9 @@ end
 --[[ Gets table from HTTP URL
 	@param String url: The desired URL
 	@return Table|boolean result|error: The content of the site parsed into a table; If the URL is not reachable, an error is printed in the terminal and boolean false is returned
---]]
-function gethttpdata(url)
-	local result = gethttpresult(url)
+]]
+function httputils.gethttpdata(url)
+	local result = httputils.gethttpresult(url)
 	if not result then 
 		return false
 	end
@@ -45,12 +51,14 @@ end
 	@param String filepath: Filepath where to create file (if file already exists, it gets overwritten)
 	@param String url: The desired URL
 	@return boolean error: If the URL is not reachable, an error is printed in the terminal and boolean false is returned; If everything goes well, true is returned
---]]
-function downloadfile(filepath,url)
-	local result = gethttpresult(url)
-	if not result then 
+]]
+function httputils.downloadfile(filepath,url)
+	local result = httputils.gethttpresult(url)
+	if not result then
 		return false
 	end
 	fileutils.storeFile(filepath,result.readAll())
 	return true
 end
+
+return httputils
